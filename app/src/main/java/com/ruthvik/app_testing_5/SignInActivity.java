f323385c1f3070e2a9f4fb118c278947b07674b9
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-04-12 22:38:15 +0530

    Updated build gradle.

---
+++ app/src/main/java/com/ruthvik/app_testing_5/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.ruthvik.app_testing_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/ruthvik/app_testing_5/SignUpActivity.java
+++ app/src/main/java/com/ruthvik/app_testing_5/SignUpActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ruthvik.app_testing_5.databinding.ActivitySignInBinding;
+import com.ruthvik.app_testing_5.databinding.ActivitySignUpBinding;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
     }
