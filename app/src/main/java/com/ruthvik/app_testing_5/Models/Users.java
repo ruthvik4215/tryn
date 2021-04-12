411280678194d738456f3b091a19a9e8ee5b6faa
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-04-12 23:25:30 +0530

    Added google authentication.

---
+++ app/src/main/java/com/ruthvik/app_testing_5/Models/Users.java
@@ -0,0 +1,71 @@
+package com.ruthvik.app_testing_5.Models;
+
+public class Users {
+    String userProfilePhoto, userName, userEmail, userPassword, userId, userLastMessage;
+
+    public Users(String userProfilePhoto, String userName, String userEmail, String userPassword, String userId, String userLastMessage) {
+        this.userProfilePhoto = userProfilePhoto;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userId = userId;
+        this.userLastMessage = userLastMessage;
+    }
+
+    public Users() {}
+
+    // signup class
+    public Users(String userName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserProfilePhoto() {
+        return userProfilePhoto;
+    }
+
+    public void setUserProfilePhoto(String userProfilePhoto) {
+        this.userProfilePhoto = userProfilePhoto;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserLastMessage() {
+        return userLastMessage;
+    }
+
+    public void setUserLastMessage(String userLastMessage) {
+        this.userLastMessage = userLastMessage;
+    }
+}
--- app/src/main/java/com/ruthvik/app_testing_5/SignUpActivity.java
+++ app/src/main/java/com/ruthvik/app_testing_5/SignUpActivity.java
@@ -1,10 +1,17 @@
 package com.ruthvik.app_testing_5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ruthvik.app_testing_5.databinding.ActivitySignInBinding;
 import com.ruthvik.app_testing_5.databinding.ActivitySignUpBinding;
 
@@ -12,6 +19,8 @@ public class SignUpActivity extends AppCompatActivity {
 
     ActivitySignUpBinding binding;
     private FirebaseAuth auth;
+    FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,24 @@ public class SignUpActivity extends AppCompatActivity {
 
         // getting the instance from firebase.
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        // onclick signup button.
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(binding.userEmail.getText().toString(), binding.userPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Account created successfully!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
