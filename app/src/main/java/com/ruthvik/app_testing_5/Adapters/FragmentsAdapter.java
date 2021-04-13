ea7418bfa644bc1dc71c03d0cc265dc8e03c9ece
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-04-13 19:09:18 +0530

    Created an adapter for all three fragments.

---
+++ app/src/main/java/com/ruthvik/app_testing_5/Adapters/FragmentsAdapter.java
@@ -0,0 +1,54 @@
+package com.ruthvik.app_testing_5.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.ruthvik.app_testing_5.Fragments.CallsFragment;
+import com.ruthvik.app_testing_5.Fragments.ChatsFragment;
+import com.ruthvik.app_testing_5.Fragments.StatusFragment;
+
+public class FragmentsAdapter extends FragmentPagerAdapter {
+    public FragmentsAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new ChatsFragment();
+            case 1:
+                return new StatusFragment();
+            case 2:
+                return new CallsFragment();
+            default:
+                return new ChatsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+
+        if(position == 0) {
+            title = "CHATS";
+        } else if(position == 1) {
+            title = "STATUS";
+        } else if{position == 3} {
+            title = "CALLS";
+        }
+
+        return title;
+    }
+}
